0260f4214e4c6f675e4ac4103dbe5296a10988d0
DimaVakhov <60767151+DimaVakhov@users.noreply.github.com>
2025-01-11 14:54:31 +0700

    Update Chapter3.java

--- Chapter3.java
+++ Chapter3.java
@@ -1,13 +1,8 @@
-import UsingRegularExpressions;
-import UsingBreakIterator;
-import UsingOpenNLP;
-import UsingStanfordPipeline;
-import UseLingPipeExamples;
-
 public class Chapter3 {
     public static void main(String[] args) {
         String paragraph = "Here is an example paragraph that will be processed.";
         
+        // Вызов методов из классов без использования импорта
         UsingRegularExpressions.process(paragraph);
         UsingBreakIterator.process(paragraph);
         UsingOpenNLP.process(paragraph, "/path/to/opennlp/models");
