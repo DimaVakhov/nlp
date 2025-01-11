a4730489289899f7c905a8eb527b04eedb54e66f
DimaVakhov <60767151+DimaVakhov@users.noreply.github.com>
2025-01-11 14:49:33 +0700

    Update Chapter3.java

--- Chapter3.java
+++ Chapter3.java
@@ -1,26 +1,17 @@
+import nlp.examples.UsingRegularExpressions;
+import nlp.examples.UsingBreakIterator;
+import nlp.examples.UsingOpenNLP;
+import nlp.examples.UsingStanfordPipeline;
+import nlp.examples.UseLingPipeExamples;
+
 public class Chapter3 {
-
-    private static String paragraph = "When determining the end of sentences "
-            + "we need to consider several factors. Sentences may end with "
-            + "exclamation marks! Or possibly questions marks? Within "
-            + "sentences we may find numbers like 3.14159, abbreviations "
-            + "such as found in Mr. Smith, and possibly ellipses either "
-            + "within a sentence …, or at the end of a sentence….";
-
     public static void main(String[] args) {
-        System.out.println("\n-=usingRegularExpressions\n");
+        String paragraph = "Here is an example paragraph that will be processed.";
+        
         UsingRegularExpressions.process(paragraph);
-
-        System.out.println("\n\n-=usingBreakIterator");
         UsingBreakIterator.process(paragraph);
-
-        System.out.println("\n \n-=usingOpenNLP");
         UsingOpenNLP.process(paragraph, "/path/to/opennlp/models");
-
-        System.out.println("\n-=usingStanfordPipeline");
         UsingStanfordPipeline.process(paragraph);
-
-        System.out.println("\n-=useLingPipeExamples");
         UseLingPipeExamples.process(paragraph);
     }
 }
